4389abaf5fc8a6314d13b30df5ff6988328a36fe
Steven Mackey <stevester94@gmail.com>
2020-11-22 23:48:16 -0800

    Begin feeble attempts at windowing

--- stream_from_dataset.py
+++ stream_from_dataset.py
@@ -38,7 +38,43 @@ def tf_deserialize_example(raw_record):
 
 
 parsed_ds = raw_dataset.map(tf_deserialize_example)
+parsed_ds = parsed_ds.prefetch(10)
 
+# So we'll accept the single unified dataset, underneath which we have our fucktons of disparate files. we'll use pre-fetching, interleaving, all that
+# special shit it make it work right, but the frontend of it will just be a regular ol dataset
+
+# This is incredibly slow
+def sliding_window_generator(parsed_ds, window_size):
+    #print("Generator entrypint")
+    for X,Y in parsed_ds:
+        #print("for loop")
+
+        window_start=0
+        window_end=window_size
+
+        X_i = X[0][0]
+        X_q = X[0][1]
+
+        while len(X_i[window_start:window_end]) == window_size:
+            #print("while loop")
+            
+            ar = np.array([   [X_i[window_start:window_end], X_q[window_start:window_end]]   ])
+            
+            yield ar, Y
+
+            window_start += 1
+            window_end   += 1
+    
+
+    print("Raise")
+    raise StopIteration
+            
+regular_count = 0
 for f1,f2 in parsed_ds:
-    print(f1)
-    print(f2)
+    regular_count += 1
+print(regular_count)
+
+windowed_count = 0
+for f1,f2 in sliding_window_generator(parsed_ds, 1000):
+    windowed_count += 1
+    print(windowed_count)
