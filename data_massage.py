969592c9af1332b6c1398850cbf6a0a2ae1a259b
Steven Mackey <stevester94@gmail.com>
2020-11-22 17:43:53 -0800

    It works but it\'s complicated, going to make it simple...

--- data_massage.py
+++ data_massage.py
@@ -84,14 +84,18 @@ def sum_even_odd(l):
         
 print("Creating list")
 
-def get_data(path):
+def get_data(path, num_floats=None):
     with open(path, "rb") as f:
-        b = f.read()
+        if num_floats == None:
+            b = f.read()
+        else:
+            b = f.read(num_floats*4)
         items = int(len(b)/4/2)
         # The strides of an array tell us how many bytes we have to skip in memory to move to the next position along a certain axis.
         return np.ndarray([1,2,items], dtype=np.float32, buffer=b, strides=(4,4,8))
 
-ld = get_data("tx_1/1589503281-0199382-ec.bin")
+print("WARNING: Only reading 10000 floats!")
+ld = get_data(sys.argv[1], 10000)
 
 #print(l[:20])
 #print(ld[0][:10])
@@ -123,18 +127,76 @@ def _int64_feature(value):
 
 t = tf.constant(ld)
 
+"""
+Creates a tf.train.Example message ready to be written to a file.
+"""
+def serialize_example(feature0, feature1, feature2):
+  # Create a dictionary mapping the feature name to the tf.train.Example-compatible
+  # data type.
+  feature = {
+      'samples': _bytes_feature(tf.io.serialize_tensor(feature0)),
+      'device_id': _int64_feature(feature1),
+      'transmission_id': _int64_feature(feature2),
+  }
+
+  # Create a Features message using tf.train.Example.
+
+    # Ugh it's a really bad naming scheme, but an example is not a single observation. In the docs they parallel arrays 10k long, being
+    # stuffed into a single example
+
+  example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+  return example_proto.SerializeToString()
+
+"""
+Wrapper to use our serialize_example in TF
+"""
+def tf_serialize_example(f0,f1,f2):
+  tf_string = tf.py_function(
+    serialize_example,
+    (f0,f1,f2),  # pass these args to the above function.
+    tf.string)      # the return type is `tf.string`.
+  return tf.reshape(tf_string, ()) # The result is a scalar
+
+# Create a dataset from tensors. This one will have only a single big fuckin observation
+features_dataset = tf.data.Dataset.from_tensor_slices(([t, t], [69, 1337], [420, 8008])) # Note this is how multiple observations would look in the dataset
+#features_dataset = tf.data.Dataset.from_tensor_slices(([t], [69], [420]))
+
+for f0,f1,f2 in features_dataset.take(1):
+  print(f0)
+  print(f1)
+  print(f2)
 
 
-example_features = tf.train.Features(feature={
-    "samples": _bytes_feature(tf.io.serialize_tensor(t)),
-    "device_id": _int64_feature(69),
-    "transmission_id": _int64_feature(420),
-})
+# The serialized version of our dataset. It's just a big fuckin string now
+serialized_features_dataset_1 = features_dataset.map(tf_serialize_example)
 
-example_proto = tf.train.Example(features=example_features)
+# Write it out to file
+filename = 'test.tfrecord_1'
+writer = tf.data.experimental.TFRecordWriter(filename)
+writer.write(serialized_features_dataset_1)
+
+"""
+This is the same fuckin thing, just using a generator
+"""
+
+"""
+def generator():
+  for features in features_dataset:
+    yield serialize_example(*features)
+serialized_features_dataset_2 = tf.data.Dataset.from_generator(
+    generator, output_types=tf.string, output_shapes=())
+
+filename = 'test.tfrecord_2'
+writer = tf.data.experimental.TFRecordWriter(filename)
+writer.write(serialized_features_dataset_2)
+"""
 
 
+# Now let's read this fuckin dataset
+filenames = [filename]
+raw_dataset = tf.data.TFRecordDataset(filenames)
 
-file_path = "muh_data"
-with tf.io.TFRecordWriter(file_path) as writer:
-    writer.write(example_proto)
+for raw_record in raw_dataset.take(1):
+  example = tf.train.Example()
+  example.ParseFromString(raw_record.numpy())
+  print(example)
